70f34d358902552ae4bd7c7a4788d379decd7dab
Ваше Имя <klypcontacts@gmail.com>
2026-08-18 15:15:05 +0200

    Fix user views, urls namespace and file encodings

--- cinema/views.py
+++ cinema/views.py
@@ -1,4 +1,4 @@
-﻿from datetime import datetime
+from datetime import datetime
 from django.db.models import Count, F
 from rest_framework import viewsets
 from rest_framework.pagination import PageNumberPagination
--- user/urls.py
+++ user/urls.py
@@ -1,4 +1,4 @@
-﻿from django.urls import path
+from django.urls import path
 from user.views import CreateUserView, CreateTokenView, ManageUserView
 
 app_name = "user"
