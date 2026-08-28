85e446869750830bd0a459269122664157a496e4
Ваше Имя <klypcontacts@gmail.com>
2026-08-28 14:04:32 +0200

    Fix settings.py long lines and remove .venv from git

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -61,14 +61,14 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilari\tyValidator"
     },
     {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
     {
-        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidat\or"
     },
     {
-        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValida\tor"
     },
 ]
 
